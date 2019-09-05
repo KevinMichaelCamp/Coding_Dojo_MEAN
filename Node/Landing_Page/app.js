455098f2d1c73b2dedad429b80326ce4a8cba9f4
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-09-04 19:02:42 -0500

    Node

---
+++ Node/Landing_Page/app.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function (request, response){
+  console.log('<^>client request URL: ', request.url);
+
+  // Routing
+  if(request.url === '/') {
+    fs.readFile('index.html', 'utf8', function(errors, contents){
+      response.writeHead(200, {'Content-type': 'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  }
+
+  else if(request.url === '/ninjas') {
+    fs.readFile('ninjas.html', 'utf8', function(errors, contents){
+      response.writeHead(200, {'Content-type': 'text/html'});
+      response.write(contents);
+      response.end();
+    })
+  }
+
+  else if(request.url === '/dojos/new') {
+    fs.readFile('dojos.html', 'utf8', function(errors, contents){
+      response.writeHead(200, {'Content-type': 'text/html'});
+      response.write(contents);
+      response.end();
+    })
+  }
+
+  else if(request.url === '/style.css') {
+    fs.readFile('style.css', 'utf8', function(errors, contents){
+      response.writeHead(200, {'Content-type': 'text/css'});
+      response.write(contents);
+      response.end();
+    })
+  }
+
+  // Error
+  else{
+    response.writeHead(404);
+    response.end('File not found!!!');
+  }
+});
+
+server.listen(6789);
+console.log("Running on localhost at port 6789 :)");
