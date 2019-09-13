6b657299e53459e8628b561ab451c738b47e1ad2
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-09-13 14:45:35 -0500

    angular

--- Mongo/REST_API/server.js
+++ Mongo/REST_API/server.js
@@ -3,7 +3,9 @@ const express = require('express'),
       path = require('path'),
       mongoose = require('./config/mongoose');
 
+
 app.use(express.json());
+app.use(express.static(__dirname + '/public/dist/public'));
 require('./config/routes')(app);
 
 app.listen(6789, () => console.log("listening on port 6789"));
